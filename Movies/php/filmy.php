6a3feb2e7baec8da6b76f0fbaca085c05eaa92b1
Michal Danihelka <54680713+erator123@users.noreply.github.com>
2022-04-21 18:12:41 +0200

    Update filmy.php

--- Movies/php/filmy.php
+++ Movies/php/filmy.php
@@ -21,7 +21,7 @@ if (!isset($pages[$p])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="../css/index.css">
     <link rel="stylesheet" href="php/DatabaseUtils.php">
     <link rel="stylesheet" href="php/kontakty.php">
     <title>filmy</title>
