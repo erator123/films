4313fd75691e8af0d88325d7ee48cfaedf49d5c0
Michal Danihelka <54680713+erator123@users.noreply.github.com>
2022-04-21 18:11:28 +0200

    Update kontakty.php

--- Movies/php/kontakty.php
+++ Movies/php/kontakty.php
@@ -22,7 +22,7 @@ if (!isset($pages[$p])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="../css/index.css">
     <link rel="stylesheet" href="php/filmy.php">
     <link rel="stylesheet" href="php/kontakty.php">
     <title>kontakty</title>
